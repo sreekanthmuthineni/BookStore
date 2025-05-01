168b4e017f3da180fe2f7357512e30b55dc3aa11
srikanth-muthineni_tmna <srikanth.muthineni@toyota.com>
2025-05-01 20:51:05 +0000

    MySqlChnages

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 	$email = $_POST['inputEmail'];
 	$pswd = $_POST['inputPasswd'];
 
-	$conn = mysqli_connect("localhost", "root", "", "www_project");
+	$conn = mysqli_connect("localhost", "root", "Admin@123", "www_project");
 	if(!$conn){
 		echo "Cannot connecto to database " . mysqli_connect_error($conn);
 		exit;
